cfde0d35c97822424a2cbbc6cf252447f8f210f0
MForever78 <MForever78@MForever78.local>
2014-12-07 14:10:00 +0800

    chagne font and add prefix

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+
+var src = {
+  css: 'assets/css/main.css',
+  images: 'assets/img/**/*'
+};
+
+var dest = {
+  css: 'css',
+  images: 'img'
+}
+
+gulp.task('css', function() {
+  return gulp.src(src.css)
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(dest.css))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest(dest.css));
+});
+
+gulp.task('img', function() {
+  return gulp.src(src.images) 
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(gulp.dest(dest.images));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(src.css, ['css']);
+  gulp.watch(src.images, ['images']);
+});
