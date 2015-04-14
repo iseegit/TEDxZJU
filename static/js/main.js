a5c3a34c7dcfd7a79db254b399672c9daea68c29
foolishguy <asasopo@126.com>
2015-04-14 10:17:39 +0800

    guest/volunteer page

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,124 @@
-console.log('This would be the main JS file.');
+$(function(){
+    $(document).ready(function() {
+
+        var mod = {};
+
+        mod.initNav = function(urlLocation){
+            $('.nav li[data-nav-target=' + urlLocation + ']').addClass('active');
+        }
+
+        mod.initVolunteerPage = function(){
+            var urlHash, currentPageVolunteers;
+            urlHash = (window.location.hash || "#2014-winter").substr(1);
+            $('.content-nav li a[data-subnav-target=' + urlHash + ']').parent().addClass('active');
+            loadVolunteers();
+            $('.content-nav li a').click(function(event) {
+                urlHash = $(event.target).attr('data-subnav-target');
+                $(event.target).parent().addClass('active').siblings().removeClass('active');
+                $('.content-wrapper').find('.guest-item').remove();
+                loadVolunteers();
+            });
+
+            $('.content-wrapper').on('click', '.volunteer-img', function(event){
+                event.preventDefault();
+                var currentVolunteerPos = $(event.currentTarget).attr('data-list-position').split('/'),
+                    currentVolunteer = list.volunteer[currentVolunteerPos[0]][currentVolunteerPos[1]];
+                var introductionDialog =$(
+                    '<div class="introductionDialog">' +
+                        '<div class="dialog">' +
+                            '<div class="content">' +
+                                '<div class="dialog-close" title="关闭窗口">X</div>' +
+                                '<img class="avatar" src="/post_image/volunteer/'+ currentVolunteerPos[0] + '/' + currentVolunteer.image + '"/>' +
+                                '<div class="intro">' +
+                                    '<div class="name">' +
+                                        currentVolunteer.name +
+                                        '<div class="sub-name">' +
+                                            currentVolunteer.pinyin +
+                                        '</div>' +
+                                    '</div>' +
+                                    '<p class="describe">' +
+                                        currentVolunteer.introduction +
+                                    '</p>' +
+                                '</div>' +
+                                '<img class="logo" src="/static/img/logo-brief.png"/>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>');
+                $('.content-wrapper').append(introductionDialog);
+                $('body').addClass('dialog-opened');
+                $('.introductionDialog .dialog-close').click(function(ev){
+                    introductionDialog.remove();
+                    $('body').removeClass('dialog-opened');
+                })
+            })
+
+            function loadVolunteers(){
+                var volunteersContentStr = '';
+                currentPageVolunteers = list.volunteer[urlHash] || [];
+                $.each(currentPageVolunteers, function(index, val) {
+                    volunteersContentStr += '<div class="guest-item"><a href="#" data-list-position="' + urlHash + '/' + index + '" class="volunteer-img"><img src="/post_image/volunteer/' + urlHash + '/' + val.image + '" alt="' + val.name + '" title="' + val.name + '"></a></div>';
+                });
+                $('.content-wrapper').append(volunteersContentStr);
+            }
+        }
+
+        mod.initGuestPage = function(){
+            var urlHash, currentPageGuests;
+            urlHash = (window.location.hash || "#2014-winter").substr(1);
+            $('.content-nav li a[data-subnav-target=' + urlHash + ']').parent().addClass('active');
+            loadGuests();
+            $('.content-nav li a').click(function(event) {
+                urlHash = $(event.target).attr('data-subnav-target');
+                $(event.target).parent().addClass('active').siblings().removeClass('active');
+                $('.content-wrapper').find('.guest-item').remove();
+                loadGuests();
+            });
+
+            function loadGuests(){
+                var guestsContentStr = '',
+                    dateMap ={
+                        'winter': '/12/01/',
+                        'summer': '/06/01/'
+                    };
+                currentPageGuests = list.guest[urlHash] || [];
+                $.each(currentPageGuests, function(index, val) {
+                    var currentGuestLink = urlHash.split('-')[0] + dateMap[urlHash.split('-')[1]] + 'guest-' + val.pinyin + '.html';
+                    guestsContentStr += '<div class="guest-item"><a href="' + currentGuestLink + '" data-list-position="'
+                                + urlHash
+                                + '/' + index + '" class="guest-img"><img src="/post_image/guest/' + urlHash + '/'
+                                + val.image + '" alt="' + val.name + '" title="' + val.name + '"></a></div>';
+                });
+                $('.content-wrapper').append(guestsContentStr);
+            }
+        }
+
+        mod.initGuestPageDuoshuoComment = function(){
+            duoshuoQuery = {short_name:"test-ted"};
+            var ds = document.createElement('script');
+            ds.type = 'text/javascript';ds.async = true;
+            ds.src = 'http://static.duoshuo.com/embed.js';
+            ds.charset = 'UTF-8';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
+        }
+
+        mod.init = function(){
+            var urlLocation = window.location.href.split('/')[3] || 'home';
+
+            mod.initNav(urlLocation);
+
+            switch(urlLocation){
+                case 'volunteer':
+                    mod.initVolunteerPage();
+                    break;
+                case 'guest':
+                    mod.initGuestPage();
+                    mod.initGuestPageDuoshuoComment();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        mod.init();
+    });
+})
---
+++ static/js/resource.js
@@ -0,0 +1,72 @@
+
+/* shall be ajax request*/
+var list = {};
+list.volunteer = {
+    '2014-winter': [
+        {
+            name: '吴旭宁',
+            pinyin: 'wuxuning',
+            image: 'wuxuning.png',
+            introduction: 'llllllllllllllllllllll'
+        },
+        {
+            name: '项羽',
+            pinyin: 'xiangyu',
+            image: 'xiangyu.png',
+            introduction: '阿飞的是打发撒旦飞洒的发'
+        }
+    ],
+    '2013-winter': [
+        {
+            name: '吴旭宁',
+            pinyin: 'wuxuning',
+            image: 'wuxuning.png',
+            introduction: 'llllllllllllllllllllll'
+        },
+        {
+            name: '项羽',
+            pinyin: 'xiangyu',
+            image: 'xiangyu.png',
+            introduction: '阿飞的是打发撒旦飞洒的发'
+        }
+    ]
+}
+
+list.guest = {
+    '2014-winter': [
+        {
+            name: '刘四',
+            pinyin: 'liusi',
+            image: 'liusi.png',
+            job: '瑞德设计有限公司创始人',
+            video: '',
+            introduction: '资深行销创新设计专家,杭州工业设计协会会长,中国十大卓越策划专家,多家知名企业商品创新及品牌行销顾问。自2009年起每年举办一次 Golden Frog Award 瑞德优秀毕业设计邀请赛,此竞赛是目前中国唯一企业主办以”非盈利社会公益活动”为特性的专项工业设计竞赛。'
+        },
+        {
+            name: '朱八',
+            pinyin: 'zhuba',
+            image: 'zhuba.png',
+            job: '瑞德设计有限公司创始人',
+            video: '',
+            introduction: '资深行销创新设计专家,杭州工业设计协会会长,中国十大卓越策划专家,多家知名企业商品创新及品牌行销顾问。自2009年起每年举办一次 Golden Frog Award 瑞德优秀毕业设计邀请赛,此竞赛是目前中国唯一企业主办以”非盈利社会公益活动”为特性的专项工业设计竞赛。'
+        }
+    ],
+    '2013-winter': [
+        {
+            name: '天才',
+            pinyin: 'tiancai',
+            image: 'tiancai.png',
+            job: '瑞德设计有限公司创始人',
+            video: '',
+            introduction: '资深行销创新设计专家,杭州工业设计协会会长,中国十大卓越策划专家,多家知名企业商品创新及品牌行销顾问。自2009年起每年举办一次 Golden Frog Award 瑞德优秀毕业设计邀请赛,此竞赛是目前中国唯一企业主办以”非盈利社会公益活动”为特性的专项工业设计竞赛。'
+        },
+        {
+            name: '黑客',
+            pinyin: 'heike',
+            image: 'heike.png',
+            job: '瑞德设计有限公司创始人',
+            video: '',
+            introduction: '资深行销创新设计专家,杭州工业设计协会会长,中国十大卓越策划专家,多家知名企业商品创新及品牌行销顾问。自2009年起每年举办一次 Golden Frog Award 瑞德优秀毕业设计邀请赛,此竞赛是目前中国唯一企业主办以”非盈利社会公益活动”为特性的专项工业设计竞赛。'
+        }
+    ]
+}
